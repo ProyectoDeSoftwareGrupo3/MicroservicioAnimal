5067d2ffd15da1c5612f3c14a0be1c2dba0b4201
Alan <159034743+AlanLeandroVargas@users.noreply.github.com>
2024-07-05 15:12:09 -0300

    Modificacion de puertos

--- AnimalRepository/Program.cs
+++ AnimalRepository/Program.cs
@@ -74,7 +74,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddHttpClient<UserApiClient>(client =>
 {
-    client.BaseAddress = new Uri("https://localhost:44350/");
+    client.BaseAddress = new Uri("https://localhost:7054/");
     client.DefaultRequestHeaders.Add("Accept", "application/json");
 });
 
